9149eb3a57fa18c7f894b81a9cb9aaed869e0761
bashar53 <bashar.iiuc77@gmail.com>
2025-08-10 15:37:39 +0600

    EmployeeCreate DTO added

---
+++ EmployeeApp/Models/DTO/EmployeeCreateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeApp.Models.DTO
+{
+    public class EmployeeCreateDTO
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+
+        public string Department { get; set; } = string.Empty;
+        public decimal Salary { get; set; }
+    }
+}
