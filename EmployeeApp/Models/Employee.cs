040e2bcfa3cc6bf7f41664edab336bff5a6220f9
bashar53 <bashar.iiuc77@gmail.com>
2025-08-10 14:39:05 +0600

    primary key added

--- EmployeeApp/Models/Employee.cs
+++ EmployeeApp/Models/Employee.cs
@@ -1,7 +1,10 @@
-﻿namespace EmployeeApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeApp.Models
 {
     public partial class Employee
     {
+        [Key]
         public int EmployeeId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Department { get; set; } = string.Empty;
