9babbcec46b3a995dd1baa0b74ea763e9c007495
bashar53 <bashar.iiuc77@gmail.com>
2025-08-10 16:15:47 +0600

    GetEmployees method added in EmployeeController

---
+++ EmployeeApp/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeApp.Data;
+using EmployeeApp.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<EmployeeDTO>> GetEmployees()
+        {   
+            return Ok(EmployeeData.EmployeeList);
+        }
+    }
+}
