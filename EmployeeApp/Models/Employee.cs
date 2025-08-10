c6f592ba79473b54416c46425fbd2dc2fe6175f2
bashar53 <bashar.iiuc77@gmail.com>
2025-08-10 19:59:23 +0600

    decimal attribute added

--- EmployeeApp/Models/Employee.cs
+++ EmployeeApp/Models/Employee.cs
@@ -10,6 +10,7 @@ namespace EmployeeApp.Models
         public int EmployeeId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Department { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Salary { get; set; }
     }
 }
