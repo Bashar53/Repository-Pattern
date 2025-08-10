4e8b79d7d3cd1b4b581b540ed3b1680889f58667
bashar53 <bashar.iiuc77@gmail.com>
2025-08-10 21:18:08 +0600

    Employee Controller Updated

---
+++ EmployeeApp/Controllers/EmployeeController.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using EmployeeApp.DbConnection;
+using EmployeeApp.Models;
+using EmployeeApp.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+        private readonly ILogger<EmployeeController> _logger;
+        protected APIResponse _response;
+        public EmployeeController(ILogger<EmployeeController> logger, ApplicationDbContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<EmployeeDTO>>> Getemployees()
+        {
+            _logger.LogInformation("Getting all Employee");
+            return Ok(await _dbContext.Employee.ToListAsync());
+        }
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]//[ProducesResponseType(400)]
+        public async Task<ActionResult<EmployeeDTO>> GetEmployees(int id)
+        {
+            if (id < 1)
+            {
+                _logger.LogError("Get employee Error with Id " + id);
+                return BadRequest("Id must be greater than 0");
+            }
+            var employee = await _dbContext.Employee.FirstOrDefaultAsync(x => x.EmployeeId == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+    }
+          
+    
+}
