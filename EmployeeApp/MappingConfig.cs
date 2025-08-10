8760b46f283867535808ecb69bbb788f3167937d
bashar53 <bashar.iiuc77@gmail.com>
2025-08-10 21:35:58 +0600

    Mapping Config updated

---
+++ EmployeeApp/MappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+namespace EmployeeApp
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            // Source -> Target
+            CreateMap<Models.Employee, Models.DTO.EmployeeDTO>().ReverseMap();
+            CreateMap<Models.Employee, Models.DTO.EmployeeCreateDTO>().ReverseMap();
+            CreateMap<Models.Employee, Models.DTO.EmployeeUpdateDTO>().ReverseMap();
+        }
+    }
+}
