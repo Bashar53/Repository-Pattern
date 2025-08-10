b5f4a2468280907f7ee578cf64ee28de23bdc3c7
bashar53 <bashar.iiuc77@gmail.com>
2025-08-10 21:23:49 +0600

    Repository Class added

---
+++ EmployeeApp/Repository/Repository.cs
@@ -0,0 +1,70 @@
+﻿using EmployeeApp.DbConnection;
+using EmployeeApp.Repository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace EmployeeApp.IRepository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        internal DbSet<T> _dbSet;
+
+        public Repository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+            _dbSet = _applicationDbContext.Set<T>();
+        }
+
+
+        public async Task CreateAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await SaveAsync();
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.FirstOrDefaultAsync(); // query will be executed here, deffered execution
+        }
+
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter = null, bool tracked = true)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.ToListAsync(); // query will be executed here, deffered execution
+
+        }
+
+        public async Task RemoveAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await SaveAsync();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _applicationDbContext.SaveChangesAsync();
+        }
+    }
+}
