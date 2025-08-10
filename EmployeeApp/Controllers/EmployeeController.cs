8eb24a67c50c98fa9aae3dd8ac531b542a0cbe2a
bashar53 <bashar.iiuc77@gmail.com>
2025-08-10 16:30:21 +0600

    parameter name updated for getEmployee by ID

--- EmployeeApp/Controllers/EmployeeController.cs
+++ EmployeeApp/Controllers/EmployeeController.cs
@@ -19,14 +19,13 @@ namespace EmployeeApp.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)] //, Type = typeof(EmployeeDTO))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]// [ProducesResponseType(404)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]//[ProducesResponseType(400)]
-        public ActionResult<EmployeeDTO> GetEmployee(int EmployeeId)
+        public ActionResult<EmployeeDTO> GetEmployee(int id)
         {
-            if (EmployeeId < 1)
+            if (id < 1)
             {
                 return BadRequest("Id must be greater than 0");
             }
-
-            var employee = EmployeeData.EmployeeList.FirstOrDefault(x => x.EmployeeId == EmployeeId);
+            var employee = EmployeeData.EmployeeList.FirstOrDefault(x => x.EmployeeId == id);
             if (employee == null)
             {
                 return NotFound();
