360bc369195e1d81359a49f4ae2569e9649cf9a3
bashar53 <bashar.iiuc77@gmail.com>
2025-08-10 14:27:33 +0600

    ApplicationDbContext class added & Employee DbSet Added

---
+++ EmployeeApp/DbConnection/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApp.DbConnection
+{
+    public partial class EmployeeDBContext : DbContext
+    {
+        public class ApplicationDbContext : DbContext
+        {
+            public ApplicationDbContext(Microsoft.EntityFrameworkCore.DbContextOptions<ApplicationDbContext> options)
+                : base(options)
+            {
+
+            }
+            public DbSet<Employee> AppResources { get; set; }
+           
+        }
+    }
+}
