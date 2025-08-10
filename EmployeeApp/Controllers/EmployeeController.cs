a93819b86c4f49c8f700595210cbd991572a9db1
bashar53 <bashar.iiuc77@gmail.com>
2025-08-10 16:26:51 +0600

    GetEmployee by Id Method added

--- EmployeeApp/Controllers/EmployeeController.cs
+++ EmployeeApp/Controllers/EmployeeController.cs
@@ -12,8 +12,26 @@ namespace EmployeeApp.Controllers
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<EmployeeDTO>> GetEmployees()
-        {   
+        {
             return Ok(EmployeeData.EmployeeList);
+        }
+        [HttpGet("{id:int}", Name = "GetEmployee")]
+        [ProducesResponseType(StatusCodes.Status200OK)] //, Type = typeof(EmployeeDTO))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]// [ProducesResponseType(404)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]//[ProducesResponseType(400)]
+        public ActionResult<EmployeeDTO> GetEmployee(int EmployeeId)
+        {
+            if (EmployeeId < 1)
+            {
+                return BadRequest("Id must be greater than 0");
+            }
+
+            var employee = EmployeeData.EmployeeList.FirstOrDefault(x => x.EmployeeId == EmployeeId);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
         }
     }
 }
