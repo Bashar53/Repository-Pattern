6d7329d4705a715717e50350807861ed0e451390
bashar53 <bashar.iiuc77@gmail.com>
2025-08-10 19:51:52 +0600

    Idenity added

--- EmployeeApp/Models/Employee.cs
+++ EmployeeApp/Models/Employee.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EmployeeApp.Models
 {
-    public partial class Employee
+    public  class Employee
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int EmployeeId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Department { get; set; } = string.Empty;
