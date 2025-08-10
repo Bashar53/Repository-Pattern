9f9fb2afa7849b57733e3765e448ff45b3783e55
bashar53 <bashar.iiuc77@gmail.com>
2025-08-10 21:32:33 +0600

    IEmployeeRepositiry Added

---
+++ EmployeeApp/Repository/IRepository/IEmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using EmployeeApp.IRepository;
+using EmployeeApp.Models;
+
+namespace EmployeeApp.Repository.IRepository
+{
+    public interface IEmployeeRepository : IRepository<Employee>
+    {
+        Task<Employee> UpdateAsync(Employee employee);
+    }
+}
