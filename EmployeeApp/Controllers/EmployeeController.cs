d9dccc68d7ffb71b35553198dc1c0b48ebe0a3b9
bashar53 <bashar.iiuc77@gmail.com>
2025-08-10 16:41:36 +0600

    CreateEmployee Post Method added

--- EmployeeApp/Controllers/EmployeeController.cs
+++ EmployeeApp/Controllers/EmployeeController.cs
@@ -1,6 +1,5 @@
 ﻿using EmployeeApp.Data;
 using EmployeeApp.Models.DTO;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeApp.Controllers
@@ -11,8 +10,8 @@ namespace EmployeeApp.Controllers
     {
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<IEnumerable<EmployeeDTO>> GetEmployees()
-        {
+        public ActionResult<IEnumerable<EmployeeDTO>> GetEmployee()
+        {   
             return Ok(EmployeeData.EmployeeList);
         }
         [HttpGet("{id:int}", Name = "GetEmployee")]
@@ -31,6 +30,44 @@ namespace EmployeeApp.Controllers
                 return NotFound();
             }
             return Ok(employee);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<EmployeeDTO> CreateEmployee([FromBody] EmployeeDTO employeeDTO)
+        {
+            // not required with data annotations?
+            // wont hit breakpoint if model is invalid
+            if (!ModelState.IsValid) // checks if the model is valid, i.e. if the data annotations are respected
+            {
+                return BadRequest(ModelState); // ModelState will contain the errors
+            }
+            // check if Employee name already exists
+            if (EmployeeData.EmployeeList.Any(x => x.Name == employeeDTO.Name))
+            {
+                ModelState.AddModelError("", "Employee name must be unique"); // key can be empty string
+                return StatusCode(StatusCodes.Status409Conflict, ModelState);
+            }
+            if (employeeDTO == null)
+            {
+                return BadRequest(employeeDTO);
+            }
+
+            if (employeeDTO.EmployeeId > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            var employeeData = new EmployeeDTO
+            {
+                EmployeeId = EmployeeData.EmployeeList.Max(x => x.EmployeeId) + 1,
+                Name = employeeDTO.Name
+            };
+            EmployeeData.EmployeeList.Add(employeeData); // EmployeeDto
+            return CreatedAtAction(nameof(GetEmployee), new { id = employeeData.EmployeeId }, employeeData);
+            // CreatedAtRoute: return CreatedAtRoute("GetEmployee", new { id = employeeData.Id }, employeeData);
+            // CreatedAtAction - returns a 201 status code and a location header with the URI of the newly created resource
         }
     }
 }
