f0f5a6150fbf3bfe30c6c586e799cbeddff7541b
bashar53 <bashar.iiuc77@gmail.com>
2025-08-10 16:04:51 +0600

    Employee data added

---
+++ EmployeeApp/Data/EmployeeData.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeApp.Models.DTO;
+
+namespace EmployeeApp.Data
+{
+    public static class EmployeeData
+    {
+        public static List<EmployeeDTO> EmployeeList = new List<EmployeeDTO>
+        {
+            new EmployeeDTO{ EmployeeId =  1,Name = "Hasan", Department="ICT",Salary=1000000 },  
+            new EmployeeDTO{ EmployeeId =  2,Name = "Rakib", Department="ICT",Salary=5000000 },  
+        };
+    }
+}
