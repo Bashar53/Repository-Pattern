acb9f9fe3683354bb9c621a6594e34e2fbd6186a
bashar53 <bashar.iiuc77@gmail.com>
2025-08-11 06:40:37 +0600

    Unit Of Work added

---
+++ EmployeeApp/Repository/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeApp.DbConnection;
+using EmployeeApp.Repository.IRepository;
+
+namespace EmployeeApp.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public IEmployeeRepository employee { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            employee = new EmployeeRepository(_db);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
