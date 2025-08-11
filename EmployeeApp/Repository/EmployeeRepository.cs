87620b75c55765fbf355a101521fc6054b1e06e3
bashar53 <bashar.iiuc77@gmail.com>
2025-08-11 06:40:24 +0600

    Employee Repository added

---
+++ EmployeeApp/Repository/EmployeeRepository.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeApp.DbConnection;
+using EmployeeApp.IRepository;
+using EmployeeApp.Models;
+using EmployeeApp.Repository.IRepository;
+
+namespace EmployeeApp.Repository
+{
+    public class EmployeeRepository : Repository<Employee>, IEmployeeRepository
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public EmployeeRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<Employee> UpdateAsync(Employee employee)
+        {
+           
+            _applicationDbContext.Employee.Update(employee);
+            await _applicationDbContext.SaveChangesAsync();
+            return employee;
+        }
+    }
+}
