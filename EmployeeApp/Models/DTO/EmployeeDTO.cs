f3376011c3e07126ee61d6f420c5e1d8cbf60037
bashar53 <bashar.iiuc77@gmail.com>
2025-08-11 06:39:11 +0600

    modified DTO

--- EmployeeApp/Models/DTO/EmployeeDTO.cs
+++ EmployeeApp/Models/DTO/EmployeeDTO.cs
@@ -6,13 +6,12 @@ namespace EmployeeApp.Models.DTO
     public class EmployeeDTO
     {
         public int EmployeeId { get; set; }
-        [Required]
-        [MaxLength(50)]
-        public string Name { get; set; } = string.Empty;
-        [Required]
+        
+        public string Name { get; set; } 
+      
 
-        public string Department { get; set; } = string.Empty;
-        [Column(TypeName = "decimal(18,2)")]
+        public string Department { get; set; }
+       
         public decimal Salary { get; set; }
     }
 }
