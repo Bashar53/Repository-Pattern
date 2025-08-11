cd48cba6f25cc7fcc2d976aae8d35ac6e12b242c
bashar53 <bashar.iiuc77@gmail.com>
2025-08-11 06:41:10 +0600

    GetEmployees , GetEmployeeById , PostEmployee added

--- EmployeeApp/Controllers/EmployeeController.cs
+++ EmployeeApp/Controllers/EmployeeController.cs
@@ -2,6 +2,7 @@
 using EmployeeApp.DbConnection;
 using EmployeeApp.Models;
 using EmployeeApp.Models.DTO;
+using EmployeeApp.Repository.IRepository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,22 +15,37 @@ namespace EmployeeApp.Controllers
     {
         private readonly ApplicationDbContext _dbContext;
         private readonly IMapper _mapper;
+        private readonly IEmployeeRepository _employeeRepository;
         private readonly ILogger<EmployeeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
         protected APIResponse _response;
-        public EmployeeController(ILogger<EmployeeController> logger, ApplicationDbContext dbContext)
+        public EmployeeController(ILogger<EmployeeController> logger, ApplicationDbContext dbContext, IEmployeeRepository employeeRepository, IUnitOfWork unitOfWork, IMapper mapper)
         {
             _logger = logger;
             _dbContext = dbContext;
+            _employeeRepository = employeeRepository;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+            _response = new();
         }
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<IEnumerable<EmployeeDTO>>> Getemployees()
+        public async Task<ActionResult<APIResponse>> GetEmployees()
         {
-            _logger.LogInformation("Getting all Employee");
-            return Ok(await _dbContext.Employee.ToListAsync());
+            _logger.LogInformation("Getting all employees");
+
+            var employee = await _unitOfWork.employee.GetAllAsync();
+            _response.Result = _mapper.Map<List<EmployeeDTO>>(employee);
+            _response.StatusCode = System.Net.HttpStatusCode.OK;
+            _response.IsSuccess = true;
+            return Ok(_response);
         }
+
+        [HttpGet("{id:int}", Name = "GetEmployeeById")]
+        [ProducesResponseType(StatusCodes.Status200OK)] //, Type = typeof(VillaDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]// [ProducesResponseType(404)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]//[ProducesResponseType(400)]
-        public async Task<ActionResult<EmployeeDTO>> GetEmployees(int id)
+        public async Task<ActionResult<EmployeeDTO>> GetEmployeeById(int id)
         {
             if (id < 1)
             {
@@ -43,7 +59,45 @@ namespace EmployeeApp.Controllers
             }
             return Ok(employee);
         }
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> CreateEmployee([FromBody] EmployeeCreateDTO createDto)
+        {
+            // not required with data annotations?
+            // wont hit breakpoint if model is invalid
+            if (!ModelState.IsValid) // checks if the model is valid, i.e. if the data annotations are respected
+            {
+                return BadRequest(ModelState); // ModelState will contain the errors
+            }
+
+            // check if villa name already exists
+            if (_dbContext.Employee.Any(x => x.Name == createDto.Name))
+            {
+                ModelState.AddModelError("", "Employee name must be unique"); // key can be empty string
+                return StatusCode(StatusCodes.Status409Conflict, ModelState);
+            }
+
+            if (createDto == null)
+            {
+                return BadRequest(createDto);
+            }
+
+            var model = _mapper.Map<Employee>(createDto);
+
+            await _unitOfWork.employee.CreateAsync(model);
+
+            _response.Result = _mapper.Map<EmployeeDTO>(model);
+            _response.StatusCode = System.Net.HttpStatusCode.Created;
+
+            return CreatedAtAction(nameof(GetEmployeeById), new { id = model.EmployeeId }, _response);
+        }
+
     }
-          
-    
 }
+
+
+
