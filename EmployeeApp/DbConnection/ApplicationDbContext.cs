c6dc802b418d7e6f17a6db2c836811c67510dc42
bashar53 <bashar.iiuc77@gmail.com>
2025-08-12 08:00:31 +0600

    AddEmployee function added for Add Employee using stored procedure

--- EmployeeApp/DbConnection/ApplicationDbContext.cs
+++ EmployeeApp/DbConnection/ApplicationDbContext.cs
@@ -1,16 +1,29 @@
 ﻿using EmployeeApp.Models;
+using EmployeeApp.Models.DTO;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeApp.DbConnection
 {
-        public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
-            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-                : base(options)
-            {
 
-            }
-            public DbSet<Employee> Employee { get; set; }
-            
         }
+        public DbSet<Employee> Employee { get; set; }
+        public async Task addEmployee(Employee _msg)
+        {
+            var nameParam = new SqlParameter("@name", _msg.Name);
+            var deptParam = new SqlParameter("@department", _msg.Department);
+            var salaryParam = new SqlParameter("@salary", _msg.Salary);
+
+            await  this.Database.ExecuteSqlRawAsync(
+                "EXEC EmployeeInsert @name, @department, @salary",
+                nameParam, deptParam, salaryParam
+            );
+        }
+
     }
+}
